f54cd36fe2717a158c96c68bb61629d93e428bfa
Déborah SOUDE <deb.soude@gmail.com>
2019-05-07 15:03:01 +0200

    Simplification du code

--- class/character.php
+++ class/character.php
@@ -8,8 +8,15 @@ class Personnage
     public $strong;
     public $maxSpeed;
 
+    public function __construct($a, $b,$c, $d) {
+        $this->level = $a;
+        $this->health = $b;
+        $this->strong = $c;
+        $this->maxSpeed = $d;
+    }
+
+    public function getHealth() {
+        return $this->health;
+    }
 };
 
-public function getHealth() {
-    return $this->health;
-}
--- index.php
+++ index.php
@@ -2,16 +2,8 @@
 
 require_once "./class/character.php";
 
-$michel = new Personnage();
-$michel->level = 10;
-$michel->health = 27;
-$michel->strong = 527;
-$michel->maxSpeed = 3;
+$michel = new Personnage(10, 27, 527, 3);
+$monique = new Personnage(8, 40, 400, 2);
 
-$monique = new Personnage();
-$monique->level = 8;
-$monique->health = 40;
-$monique->strong = 400;
-$monique->maxSpeed = 2;
 
 echo $michel->getHealth();
